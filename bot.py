94a899b617d3295f168773528091a7a2acd28f61
Leonard Rapp <leonardrapp@gmx.de>
2019-04-17 23:16:50 +0200

    add start command function and some comments

--- bot.py
+++ bot.py
@@ -5,12 +5,20 @@ def hello(bot, update):
     update.message.reply_text('Hello {}'.format(update.message.from_user.first_name))
 
 
+def start(bot, update):
+    update.message.reply_text('Welcome to SkylinesBot!')
+
+
 # read secret bot token from file, remove newline
 token_file = open('token.txt', 'r')
 secret_token = token_file.read()[:-1]
 
-updater = Updater(secret_token)
+# init updater
+updater = Updater(token=secret_token)
+# register command handler for above command functions
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
+updater.dispatcher.add_handler(CommandHandler('start', start))
 
+# start bot
 updater.start_polling()
 updater.idle()
