421e8965730befa65c3b94fa48ef49e9d737ac93
Leonard Rapp <leonardrapp@gmx.de>
2019-04-17 20:24:35 +0200

    read secret bot token from token.txt

--- bot.py
+++ bot.py
@@ -5,8 +5,11 @@ def hello(bot, update):
     update.message.reply_text('Hello {}'.format(update.message.from_user.first_name))
 
 
-updater = Updater('TOKEN')
+# read secret bot token from file, remove newline
+token_file = open('token.txt', 'r')
+secret_token = token_file.read()[:-1]
 
+updater = Updater(secret_token)
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
 
 updater.start_polling()
